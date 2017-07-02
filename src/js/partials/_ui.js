40fb576659480cb625fb6163130fc0cf675090c9
Helen Zhou <helen.zhou6@gmail.com>
2017-07-02 16:41:48 +0100

    Refactoring oyster/discount card selection.

--- src/js/partials/_ui.js
+++ src/js/partials/_ui.js
@@ -533,24 +533,29 @@ export default function ui() {
     });
 
     // THE UI TO NOT ALLOW CERTAIN OYSTER CARDS WITH OTHER RAILCARDS
+		// Caches the DOM elements
+		const $discountCard = $('.js-discount-card-select');
+		const $discountCardInput = $('#discount-card');
+		const $oysterCard = $('.js-oyster-card-select');
+		const $childOysterCard = $oysterCard.find('option[value="child-jobless"]');
+		const $childDiscountCard = $discountCard.find('option[value="child-jobless"]');
+		const $studentOysterCard = $oysterCard.find('option[value="student"]');
+
+		// Removes any existing elements disabled
 		const removeDisabled = function(card, $this) {
-			// Removes any existing disabled
 			card.find('option:disabled').prop('disabled', '');
 		};
 
     $('.js-oyster-card-select').change(function() {
-      const $discountCard = $('.js-discount-card-select');
-			const $discountCardInput = $('#discount-card');
+			const selectedOyster = $(this).find("option:selected").val();
 
       removeDisabled($discountCard);
 
       $discountCardInput.prop('disabled', false);
       $discountCard.removeClass('is-disabled');
 
-      const selectedOyster = $(this).find("option:selected").val();
-
       if (selectedOyster === 'student') {
-        $discountCard.find('option[value="child-jobless"]').prop('disabled', true);
+        $childDiscountCard.prop('disabled', true);
       } else if (selectedOyster === 'child-jobless') {
         $discountCardInput.prop('disabled', true);
         $discountCard.addClass('is-disabled');
@@ -558,20 +563,17 @@ export default function ui() {
     });
 
     $('.js-discount-card-select').change(function() {
-      const $oysterCard = $('.js-oyster-card-select');
-      const childOysterCard = $oysterCard.find('option[value="child-jobless"]');
+			const selectedDiscount = $(this).find("option:selected").val();
 
 			removeDisabled($oysterCard);
 
-      const selectedDiscount = $(this).find("option:selected").val();
-
       if (selectedDiscount === 'railcard') {
-        childOysterCard.prop('disabled', true);
+        $childOysterCard.prop('disabled', true);
       } else if (selectedDiscount === 'child-jobless') {
-        $oysterCard.find('option[value="student"]').prop('disabled', true);
-        childOysterCard.prop('disabled', true);
+        $studentOysterCard.prop('disabled', true);
+        $childOysterCard.prop('disabled', true);
       } else if (selectedDiscount === 'disabled') {
-        childOysterCard.prop('disabled', true);
+        $childOysterCard.prop('disabled', true);
       }
     });
 
